1dd2b3d404ddb74195686774c356aade41100b38
Einav Peretz-Andersson <einav.peretz.andersson@ju.se>
2026-01-23 09:57:02 +0100

    Fix js/icons.js exports

--- js/icons.js
+++ js/icons.js
@@ -1,3 +1,9 @@
-export { Icons };
-export default Icons;
+// Compatibility wrapper for legacy imports of /js/icons.js
 
+import Icons from "./components/icons.js";
+
+// Named export (for: import { Icons } from "/js/icons.js")
+export { Icons };
+
+// Default export (for: import Icons from "/js/icons.js")
+export default Icons;
